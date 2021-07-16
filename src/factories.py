fc755b5cc06c742def9c6b794559f565323bb783
Václav Dohnal <vaclav.dohnal@gmail.com>
2021-07-16 11:00:25 +0200

    Safely import Triton client

--- src/factories.py
+++ src/factories.py
@@ -8,9 +8,12 @@ from functools import cache
 from typing import Final
 
 import httpx
-from tritonclient.http import InferenceServerClient
-
 from src.dependencies import get_settings
+
+try:
+    from tritonclient.http import InferenceServerClient
+except ImportError:
+    InferenceServerClient = object()
 
 logger: Final = logging.getLogger(__name__)
 
@@ -21,4 +24,5 @@ async def make_httpx_client():
 
 @cache
 def make_triton_client() -> InferenceServerClient:
+    from tritonclient.http import InferenceServerClient
     return InferenceServerClient(url=get_settings().triton_service_host)
--- src/inference/triton.py
+++ src/inference/triton.py
@@ -4,16 +4,29 @@
 from __future__ import annotations
 
 import logging
+import platform
 from typing import BinaryIO, Final
 
 import numpy as np
-from tritonclient.http import InferInput, InferRequestedOutput
+from PIL import Image
 
 from src.factories import make_triton_client
 
-from PIL import Image
-
 logger: Final = logging.getLogger(__name__)
+
+try:
+    from tritonclient.http import InferInput, InferRequestedOutput
+except ImportError as e:
+    if platform.system() == "Windows":
+        logger.warning(
+            "Triton is not available on Windows. "
+            "Inference will not work on this server, "
+            "but at least the whole project will run."
+        )
+        InferInput = object()
+        InferRequestedOutput = object()
+    else:
+        raise RuntimeError("Failed to import Triton client") from e
 
 
 async def triton_tensorflow_inference(image_content: BinaryIO, **kwargs):
@@ -26,7 +39,7 @@ async def triton_tensorflow_inference(image_content: BinaryIO, **kwargs):
     infer_input = InferInput("input_1", [1, 224, 224, 3], "FP32")
     infer_input.set_data_from_numpy(jpeg_rgb)
     output = InferRequestedOutput("activation_49")
-    response = triton_client.infer("resnet-50-tensorflow", model_version='1', inputs=[infer_input], outputs=[output])
+    response = triton_client.infer("resnet-50-tensorflow", model_version="1", inputs=[infer_input], outputs=[output])
 
     assert response.as_numpy("activation_49").tolist()
 
@@ -36,14 +49,12 @@ async def triton_pytorch_inference(image_content: BinaryIO, **kwargs):
 
     jpeg_rgb = Image.open(image_content).convert("RGB")
     jpeg_rgb.thumbnail((224, 224))
-    normalized_jpeg = (np.array(jpeg_rgb) - np.array([0.485, 0.456, 0.406])) / np.array(
-        [0.229, 0.224, 0.225]
-    )
+    normalized_jpeg = (np.array(jpeg_rgb) - np.array([0.485, 0.456, 0.406])) / np.array([0.229, 0.224, 0.225])
     normalized_jpeg = np.expand_dims(np.einsum("ijk->kij", np.array(normalized_jpeg)), 0).astype(np.float32)
 
     infer_input = InferInput("input__0", [1, 3, 224, 224], "FP32")
     infer_input.set_data_from_numpy(normalized_jpeg)
     output = InferRequestedOutput("output__0")
-    response = triton_client.infer("resnet-50-torch", model_version='1', inputs=[infer_input], outputs=[output])
+    response = triton_client.infer("resnet-50-torch", model_version="1", inputs=[infer_input], outputs=[output])
 
     assert response.as_numpy("output__0").tolist()
