79393cc1198868b07815e205c1c87dfb1440dbb2
Václav Dohnal <vaclav.dohnal@gmail.com>
2021-07-16 11:31:05 +0200

    Fix local data loading

--- src/dependencies.py
+++ src/dependencies.py
@@ -4,15 +4,18 @@
 from __future__ import annotations
 
 import functools
+import logging
 import os
 import random
 from pathlib import Path
-from typing import List
+from typing import Final, List
 
 import httpx
 from starlette.requests import Request
 
 from src.config import Settings
+
+logger: Final = logging.getLogger(__name__)
 
 
 def httpx_client(request: Request) -> httpx.AsyncClient:
@@ -24,12 +27,12 @@ def datadir() -> Path:
     if DATA_DIR := os.environ.get("DATA_DIR"):
         return Path(DATA_DIR).resolve(strict=True)
     else:
-        return Path.cwd() / 'data'
+        return Path.cwd() / "data"
 
 
 @functools.cache
 def images_in_datadir() -> List[Path]:
-    return list((datadir() / "imagenet").glob("*.jpeg"))
+    return list((datadir() / "imagenet").glob("*.JPEG"))
 
 
 def get_random_image() -> Path:
