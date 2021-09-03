0eb083d0e69e69bdccd370c103f9398a67d247e6
Václav Dohnal <vaclav.dohnal@gmail.com>
2021-09-03 15:38:58 +0200

    Refactor grpc

--- src/grpc_router.py
+++ src/grpc_router.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 import io
 import logging
 from pathlib import Path
-from typing import cast, Final
+from typing import BinaryIO, cast, Final
 
 import aiofiles
 import grpc
@@ -59,11 +59,7 @@ async def _(
     response_class=PlainTextResponse,
     summary="Performs GRPC inference for an image",
 )
-async def _(
-    *,
-    serving_type: Servings,
-    random_image: Path = Depends(get_random_image)
-) -> str:
+async def _(*, serving_type: Servings, random_image: Path = Depends(get_random_image)) -> str:
     """
     Performs inference for the input image (JPEG).
     Based on the value of `serving_type`, it selects the correct
@@ -76,29 +72,34 @@ async def _(
         image_content = io.BytesIO(await f.read())
 
     if serving_type == Servings.torchserve:
-        async with grpc.aio.insecure_channel("localhost:7070") as channel:
-            stub = inference_pb2_grpc.InferenceAPIsServiceStub(channel)
-            input_data = {"data": image_content.read()}
-            response = await stub.Predictions(
-                inference_pb2.PredictionsRequest(model_name="resnet-50", input=input_data)
-            )
-            debug(response)
-
+        await torchserve_grpc(image_content)
     elif serving_type == Servings.tfserving:
-        async with grpc.aio.insecure_channel("localhost:9000") as channel:
-            stub_tf = prediction_service_pb2_grpc.PredictionServiceStub(channel)
-
-            request = predict_pb2.PredictRequest()
-            request.model_spec.name = "resnet_50_classification"
-            request.model_spec.signature_name = "serving_default"
-
-            jpeg_rgb = Image.open(image_content).convert("RGB")
-            jpeg_rgb = np.expand_dims(np.array(jpeg_rgb) / 255.0, 0)
-            jpeg_rgb = jpeg_rgb.astype(np.float32)
-
-            request.inputs["input_1"].CopyFrom(make_tensor_proto(jpeg_rgb))
-
-            response = await stub_tf.Predict(request)
-            debug(response)
+        await tfserving_grpc(image_content)
 
     return "OK"
+
+
+async def tfserving_grpc(image_content: BinaryIO) -> None:
+    async with grpc.aio.insecure_channel("localhost:9000") as channel:
+        stub_tf = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+
+        request = predict_pb2.PredictRequest()
+        request.model_spec.name = "resnet_50_classification"
+        request.model_spec.signature_name = "serving_default"
+
+        jpeg_rgb = Image.open(image_content).convert("RGB")
+        jpeg_rgb = np.expand_dims(np.array(jpeg_rgb) / 255.0, 0)
+        jpeg_rgb = jpeg_rgb.astype(np.float32)
+
+        request.inputs["input_1"].CopyFrom(make_tensor_proto(jpeg_rgb))
+
+        response = await stub_tf.Predict(request)
+        debug(response)
+
+
+async def torchserve_grpc(image_content: BinaryIO) -> None:
+    async with grpc.aio.insecure_channel("localhost:7070") as channel:
+        stub = inference_pb2_grpc.InferenceAPIsServiceStub(channel)
+        input_data = {"data": image_content.read()}
+        response = await stub.Predictions(inference_pb2.PredictionsRequest(model_name="resnet-50", input=input_data))
+        debug(response)
